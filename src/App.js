c2e9f36df58c11e4a656ceef695090184bd616d1
aayusrsta <69640693+aayusrsta@users.noreply.github.com>
2023-06-01 11:17:38 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import './App.css';
-import SignIn from './components/Form/form';
-import AuthDetails from './components/authDetails';
+import "./App.css";
+import SignIn from "./components/Form/form";
+import AuthDetails from "./components/authDetails";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-
 
 function App() {
   return (
     <Router>
-
-   <Routes>
-   <Route path="/" element={<SignIn />} />
+      <Routes>
+        <Route path="/" element={<SignIn />} />
         <Route path="/auth-details" element={<AuthDetails />} />
-    </Routes>
+      </Routes>
     </Router>
   );
 }
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,51 +1,3 @@
-// import { signInWithEmailAndPassword } from "firebase/auth";
-// import React, { useState } from "react";
-// import { auth } from "../Form/firebase";
-// import "./form.css"
-
-// const SignIn = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const signIn = (e) => {
-//     e.preventDefault();
-//     signInWithEmailAndPassword(auth, email, password)
-//       .then((userCredential) => {
-//         console.log(userCredential);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-
-//   return (
-//     <div className="container">
-//       <form onSubmit={signIn}>
-//         <h3>Login to the account</h3>
-//         <div className="loginCard">
-//         <input
-//           type="email"
-//           placeholder="Enter your email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         ></input><br/><br/>
-//         <input
-//           type="password"
-//           placeholder="Enter your password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         ></input><br/>
-//         <button type="submit" className="customButton">Log In</button>
-//         </div>
-
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default SignIn;
-
-// SignIn.js
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -62,7 +14,7 @@ const SignIn = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
-        navigate("/auth-details"); // Navigate to AuthDetails page
+        navigate("/auth-details"); 
       })
       .catch((error) => {
         console.log(error);
@@ -99,7 +51,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-
-
-
